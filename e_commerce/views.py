fb6edb46799ea0a3e332fbceb8a61959d49b392b
yinnihisoka <yinnihisoka@gmail.com>
2020-01-02 20:26:17 -0300

    home_page.html

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
 def home_page(request):
-    return HttpResponse("Olá Mundo")
+    return render(request, "home_page.html", {})
